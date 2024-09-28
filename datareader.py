dab51b496ac15ddd1f82f43c6b1eb198c971aefe
David Hintelmann <davidhin@mac.com>
2024-09-28 15:41:48 -0400

    add notebook mod datareader, README

--- datareader.py
+++ datareader.py
@@ -3,40 +3,43 @@ import requests
 from bs4 import BeautifulSoup
 
 """
-Get a list of options from https://m-x.ca/nego_liste_en.php
+Get a list of options from https://www.m-x.ca/en/trading/data/options-list
 TMX website
 
-Index and ETF options
+ETF options
+Index options
 Equity options
 Currency options
 Weekly options
 """
 def get_list(market=str) -> pd.DataFrame:
-    tmx = "https://m-x.ca/nego_liste_en.php" # TMX website, where data is taken from
+    tmx = 'https://www.m-x.ca/en/trading/data/options-list' # TMX website, where data is taken from
 
-    #check that parameter is of type string
+    # check that parameter is of type string
     is_str1 = isinstance(market, str)
     if not is_str1:
-        raise TypeError("market parameter must be of type string")
+        raise TypeError('market parameter must be of type string')
 
-    #now get data from https://m-x.ca/nego_liste_en.php TMX website
+    # now get data from https://www.m-x.ca/en/trading/data/options-list TMX website
     try:
-        market = market.lower()
+        market = '#'+ market.lower()
+        df = pd.read_html(tmx+market)
     except Exception as e:
         print(e)
     else:
-        if market == 'index' or market == 'etf':
-            market = 0
-        elif market == 'equity':
-            market = 1
-        elif market == 'currency':
-            market = 2
-        elif market == 'weekly':
-            market = 3
-        else:
-            raise Exception("Did not enter market type, choose from Index or ETF, Equity, Currency, Weekly.")
-        df = pd.read_html(tmx)
-        return df[market]
+        match market:
+            case '#etf':
+                return df[0]
+            case '#index':
+                return df[1]
+            case '#equity':
+                return df[2]
+            case '#currency':
+                return df[3]
+            case '#weekly':
+                return df[4]
+            case _:
+                return df
 
 """
 Get options prices at predetermined dates from TMX website
@@ -47,44 +50,32 @@ open interest
 implied volatility
 volume
 """
-def get(market=str,ticker_symbol=str) -> pd.DataFrame:
-    tmx = "https://m-x.ca/nego_cotes_en.php" # TMX website, where data is taken from
+def get(ticker_symbol=str) -> pd.DataFrame:
+    tmx = 'https://www.m-x.ca/en/trading/data/quotes' # TMX website, where data is taken from
 
-    #check that both parameters are of type string
+    # check that both parameters are of type string
     is_str1 = isinstance(ticker_symbol, str)
-    is_str2 = isinstance(market, str)
-    if not (is_str1 and is_str2):
-        raise TypeError("market & ticker_symbol parameters must be of type string")
+    if not is_str1:
+        raise TypeError('ticker_symbol parameters must be of type string')
 
-    #now get data from https://m-x.ca/nego_liste_en.php TMX website
+    # now get data from https://www.m-x.ca/en/trading/data/quotes TMX website
     try:
-        market = market.lower()
         ticker_symbol = ticker_symbol.upper()
+        url = tmx + '?symbol=' + ticker_symbol + '*#quotes'
+        df = pd.read_html(url)[0].iloc[:-1] # do not include last row, rubbish information
     except Exception as e:
         print(e)
     else:
-        if market == ('index' or 'etf'):
-            market = 0
-        elif market == 'equity':
-            market = 1
-        elif market == 'currency':
-            market = 2
-        elif market == 'weekly':
-            market = 3
-        else:
-            raise Exception("Did not enter market type, choose from Index or ETF, Equity, Currency, Weekly.")
-        url = tmx + '?symbol=' + ticker_symbol + '*'
-        df = pd.read_html(url)
-        df[0].rename(columns={'Bid price.1':'Bid price_', 'Ask price.1':'Ask price_', 'Last Price.1':'Last Price_',
-                             'Impl. vol..1':'Impl. vol_', 'Open int..1':'Open int_', 'Vol..1':'Vol_'}, inplace=True)
         return df[0].iloc[:-1] #do not include last row, rubbish information
 
 """
 Get stock price from TMX to compare to strike price
 can accept string or list of strings
+
+!!! need to update function since tmx url has changed !!!
 """
 def get_stock(ticker_symbol=str) -> pd.DataFrame:
-    tmx = "https://m-x.ca/nego_cotes_en.php" # TMX website, where data is taken from
+    tmx = 'https://www.m-x.ca/en/trading/data/quotes' # TMX website, where data is taken from
 
     #check that parameter is of type string
     is_str1 = checktype(ticker_symbol) #isinstance(ticker_symbol, str)
